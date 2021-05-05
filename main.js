73264e8bc48c1359463ccaccb4c3ee03356923be
MADHU963 <66484877+MADHU963@users.noreply.github.com>
2021-05-05 19:41:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Redirect1() {
+    window.location = " PW.html";
+}
+
+function Redirect2() {
+    window.location = " clock.html";
+}
+
+function Redirect3() {
+    window.location = " PB.html";
+}
+
+function Redirect4() {
+    window.location = " PF.html";
+}
